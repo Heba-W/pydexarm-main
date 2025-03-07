79663d37b0169ea8fb4403f5b8596f476aa1b179
Heba Wali <26.hebawali@gmail.com>
2025-03-07 09:49:28 -0600

    first commit

---
+++ example.py
@@ -0,0 +1,24 @@
+from pydexarm import Dexarm
+import time
+
+'''windows'''
+dexarm = Dexarm(port="COM6")
+'''mac & linux'''
+# device = Dexarm(port="/dev/tty.usbmodem3086337A34381")
+
+dexarm.go_home()
+
+dexarm.move_to(50, 300, 0)
+dexarm.move_to(50, 300, -65)
+dexarm.air_picker_pick()
+dexarm.move_to(50, 300, 0)
+dexarm.move_to(-50, 300, 0)
+dexarm.move_to(-50, 300, -65)
+dexarm.air_picker_place()
+dexarm.air_picker_stop()
+time.sleep(3)
+
+dexarm.go_home()
+
+
+dexarm.close()
